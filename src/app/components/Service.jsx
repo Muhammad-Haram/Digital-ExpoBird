a6286f8748ee5d3adc141e07d8b3529bb2f4178e
Abdul Rehman <arboacha@gmail.com>
2024-04-03 11:09:12 +0500

    change

--- src/app/components/Service.jsx
+++ src/app/components/Service.jsx
@@ -17,7 +17,7 @@ const Service = () => {
             <div className="col-span-6 left-container">
               <div className="md:pt-0 md:h-auto md:mb-[200px] lg:h-auto lg:mb-[200px]">
                 <div className='ml-[20px]'>
-                  <video className="py-3 ml-[150px]" alt="" autoPlay={true}
+                  <video className="py-3 ml-[100px] sm:ml-0" alt="" autoPlay={true}
                     muted={true}
                     loop={true}
                     src="/assets/video-editor.mp4" type="video/webm" >
@@ -26,7 +26,7 @@ const Service = () => {
                 </div>
               </div>
             </div>
-            <div className="col-span-6">
+            <div className="col-span-6 w-[700px]">
               <div className="pt-[8rem] p-16 md:h-auto md:mb-[100px] lg:h-auto lg:mb-[200px]">
 
                 <div className='flex justify-center'>
@@ -45,16 +45,16 @@ const Service = () => {
               </div>
             </div>
           
-            <div className="col-span-6">
+            <div className="col-span-6 w-[700px]">
               <div className="pt-[8rem] p-16 md:h-auto md:mb-[100px] lg:h-auto lg:mb-[200px]">
 
                 <div className='flex justify-center'>
                   <div>
-                    <Image src="/assets/service-animation-07.png" height={0} width={100} className="py-3" alt="" />
+                    <Image src="/assets/service-animation-01.png" height={0} width={100} className="py-3" alt="" />
                   </div>
                   <div>
-                    <p className="text-[25px] text-[#252E48] font-bold pl-5 md:text-center" data-inviewport >Web Development</p>
-                    <p className="text-[18px] text-[#545F7E] max-w-[70%] pl-5 py-3 md:text-center">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
+                    <p className="sm:text-center text-[25px] text-[#252E48] font-bold pl-5 md:text-center" data-inviewport >Mobile App Development</p>
+                    <p className="text-[18px] text-[#545F7E] max-w-[70%] pl-5 py-3 md:text-center sm:text-center">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
                     <p className="text-[18px] text-[#252E48] font-medium max-w-[70%] pl-5 py-3 md:text-center">Replaces: Your website subscription
                     </p>
                   </div>
@@ -79,7 +79,7 @@ const Service = () => {
             <div className="col-span-6 left-container">
               <div className=" md:pt-0  md:h-auto md:mb-[200px] lg:h-auto lg:mb-[200px]">
                 <div>
-                  <video height={0} width={600} className="py-3 ml-[150px]" alt="" autoPlay={true}
+                  <video height={0} width={600} className="py-3 ml-[100px] sm:ml-0" alt="" autoPlay={true}
                     muted={true}
                     loop={true}
                     src="/assets/website-maker.mp4" type="video/webm" >
@@ -89,15 +89,15 @@ const Service = () => {
               </div>
             </div>
 
-            <div className="col-span-6">
+            <div className="col-span-6 w-[700px]">
               <div className="pt-[8rem] p-16 md:h-auto md:mb-[100px] lg:h-auto lg:mb-[200px]">
 
                 <div className='flex justify-center'>
                   <div>
-                    <Image src="/assets/service-animation-07.png" height={0} width={100} className="py-3" alt="" />
+                    <Image src="/assets/service-animation-02.png" height={0} width={100} className="py-3" alt="" />
                   </div>
                   <div>
-                    <p className="text-[25px] text-[#252E48] font-bold pl-5 md:text-center" data-inviewport >Web Development</p>
+                    <p className="text-[25px] text-[#252E48] font-bold pl-5 md:text-center" data-inviewport >Digital Marketing</p>
                     <p className="text-[18px] text-[#545F7E] max-w-[70%] pl-5 py-3 md:text-center">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
                     <p className="text-[18px] text-[#252E48] font-medium max-w-[70%] pl-5 py-3 md:text-center">Replaces: Your website subscription
                     </p>
