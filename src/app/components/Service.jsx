ddcd918933daa85c78b5726eec70ff5b2ee823e4
Muhammad Haram <haramh643@gmail.com>
2024-04-03 12:40:24 +0100

    commit

--- src/app/components/Service.jsx
+++ src/app/components/Service.jsx
@@ -14,9 +14,10 @@ const Service = () => {
           </div>
 
           <div className=" grid grid-cols-12 sm:grid sm:grid-cols-1">
+
             <div className="col-span-6 left-container">
               <div className="md:pt-0 md:h-auto md:mb-[200px] lg:h-auto lg:mb-[200px]">
-                <div className='ml-[20px]'>
+                <div className='sm:ml-[0px] ml-[20px]'>
                   <video className="py-3 ml-[100px] sm:ml-0" alt="" autoPlay={true}
                     muted={true}
                     loop={true}
@@ -26,18 +27,21 @@ const Service = () => {
                 </div>
               </div>
             </div>
-            <div className="col-span-6 w-[700px]">
-              <div className="pt-[8rem] p-16 md:h-auto md:mb-[100px] lg:h-auto lg:mb-[200px]">
+
+            <div className="sm:mb-[30px] sm:w-[350px] col-span-6 w-[700px]">
+              <div className="sm:p-2 pt-[8rem] p-16 md:h-auto md:mb-[100px] lg:h-auto lg:mb-[200px]">
 
                 <div className='flex justify-center'>
                   <div>
                     <Image src="/assets/service-animation-07.png" height={0} width={100} className="py-3" alt="" />
                   </div>
                   <div>
+
                     <p className="text-[25px] text-[#252E48] font-bold pl-5 md:text-center" data-inviewport >Web Development</p>
-                    <p className="text-[18px] text-[#545F7E] max-w-[70%] pl-5 py-3 md:text-center">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
+                    <p className="sm:text-[13px] text-[18px] text-[#545F7E] max-w-[70%] pl-5 py-3 md:text-center">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
                     <p className="text-[18px] text-[#252E48] font-medium max-w-[70%] pl-5 py-3 md:text-center">Replaces: Your website subscription
                     </p>
+
                   </div>
                 </div>
                 <div className="bg-white shadow-xl p-4 rounded-xl">
@@ -45,8 +49,8 @@ const Service = () => {
               </div>
             </div>
 
-            <div className="col-span-6 w-[700px] sm:ml-0 ml-[100px]">
-              <div className="pt-[8rem] p-16 md:h-auto md:mb-[100px] lg:h-auto lg:mb-[200px]">
+            <div className="sm:mb-[30px] sm:w-[350px] col-span-6 w-[700px] sm:ml-0 ml-[100px]">
+              <div className="sm:p-2 pt-[8rem] p-16 md:h-auto md:mb-[100px] lg:h-auto lg:mb-[200px]">
 
                 <div className='flex justify-center'>
                   <div>
@@ -54,7 +58,7 @@ const Service = () => {
                   </div>
                   <div>
                     <p className="text-[25px] text-[#252E48] font-bold pl-5 md:text-center" data-inviewport >Mobile App Development</p>
-                    <p className="text-[18px] text-[#545F7E] max-w-[70%] pl-5 py-3 md:text-center sm:text-center">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
+                    <p className="sm:text-[13px] text-[18px] text-[#545F7E] max-w-[70%] pl-5 py-3 md:text-center">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
                     <p className="text-[18px] text-[#252E48] font-medium max-w-[70%] pl-5 py-3 md:text-center">Replaces: Your website subscription
                     </p>
                   </div>
@@ -65,7 +69,7 @@ const Service = () => {
             </div>
 
             <div className="col-span-6 left-container">
-              <div className=" md:pt-0  md:h-auto md:mb-[200px] lg:h-auto lg:mb-[200px]">
+              <div className=" md:pt-0 md:h-auto md:mb-[200px] lg:h-auto lg:mb-[200px]">
                 <div>
                   <video height={0} width={600} className="py-3" alt="" autoPlay={true}
                     muted={true}
@@ -76,8 +80,9 @@ const Service = () => {
                 </div>
               </div>
             </div>
+
             <div className="col-span-6 left-container">
-              <div className=" md:pt-0  md:h-auto md:mb-[200px] lg:h-auto lg:mb-[200px]">
+              <div className=" md:pt-0 md:h-auto md:mb-[200px] lg:h-auto lg:mb-[200px]">
                 <div>
                   <video height={0} width={600} className="py-3 ml-[100px] sm:ml-0" alt="" autoPlay={true}
                     muted={true}
@@ -89,8 +94,8 @@ const Service = () => {
               </div>
             </div>
 
-            <div className="col-span-6 w-[700px]">
-              <div className="pt-[8rem] p-16 md:h-auto md:mb-[100px] lg:h-auto lg:mb-[200px]">
+            <div className="sm:mb-[30px] sm:w-[350px] col-span-6 w-[700px]">
+              <div className="sm:p-2 pt-[8rem] p-16 md:h-auto md:mb-[100px] lg:h-auto lg:mb-[200px]">
 
                 <div className='flex justify-center'>
                   <div>
@@ -98,7 +103,7 @@ const Service = () => {
                   </div>
                   <div>
                     <p className="text-[25px] text-[#252E48] font-bold pl-5 md:text-center" data-inviewport >Digital Marketing</p>
-                    <p className="text-[18px] text-[#545F7E] max-w-[70%] pl-5 py-3 md:text-center">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
+                    <p className="sm:text-[13px] text-[18px] text-[#545F7E] max-w-[70%] pl-5 py-3 md:text-center">We imagine and build experiences and businesses that win hearts and realize the future. Explore how we work.</p>
                     <p className="text-[18px] text-[#252E48] font-medium max-w-[70%] pl-5 py-3 md:text-center">Replaces: Your website subscription
                     </p>
                   </div>
