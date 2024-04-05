5b582a7437bc0257c843270c366d5276ea9bfa5f
Abdul Rehman <arboacha@gmail.com>
2024-04-05 10:00:55 +0500

    changes

--- src/app/components/FeatureWork.js
+++ src/app/components/FeatureWork.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
 
-const FeatureWork = ({ title }) => {
+const FeatureWork = () => {
   return (
     <>
-      <div className="bg-[#000213] font-montserrat">
+      <div className="bg-[#000213] font-montserrat overflow-hidden">
         <div className="xl:max-w-[1440px] mx-auto">
           <div className="p-[9rem] sm:p-10">
             <div className="sm:grid-cols-1 md:grid md:grid-cols-1 grid grid-cols-2 items-center">
-              <h6 className="sm:text-[40px] sm:text-center sm:mt-10 sm:mb-6 md:text-[60px] sm:space-x-10 text-[85px] text-[#FFFFFF] max-w-[400px]  leading-none font-bold sm:pb-20 pb-24">
+              <h6 className="sm:text-[40px] lg:ml-[-20px] sm:text-center sm:mt-10 sm:mb-6 md:text-[60px] sm:space-x-10 sm:flex sm:items-center sm:m-auto text-[80px] text-[#FFFFFF] max-w-[400px]  leading-none font-bold sm:pb-20 pb-24">
                 Featured works
                 <span>
                   <div className="sm:hidden ml-[0px] sm:right-[45px] right-[25px] sm:mt-[20px] md:ml-12 md:mt-4 ico animated">
@@ -36,29 +36,28 @@ const FeatureWork = ({ title }) => {
                   </div>
                 </span>
               </h6>
-
-              <p className="sm:-mt-20 sm:text-center sm:text-[20px] text-[25px] text-white max-w-[450px] pb-24">{title}</p>
+              <p className="sm:-mt-20 sm:text-center sm:text-[20px] text-[25px] text-white max-w-[450px] pb-24 sm:flex sm:items-center sm:m-auto">We are a team of design and research experts with over ten years of experience in improving businesses and peoples lives through smart design solutions</p>
             </div>
             <div className="sm:grid sm:grid-cols-2 grid grid-cols-2 gap-8">
               <div className="max-w-[605px]">
-                <Image src={"/assets/work01.png"} width={450} height={580} className="rounded-[50px] sm:rounded-[20px]" alt="pic" />
-                <p className="sm:text-center ml-[137px] sm:leading-none text-white sm:text-[20px] mt-8 font-montserrat text-[25px] font-semibold">Zorro University</p>
+                <Image src={"/assets/work01.png"} width={350} height={0} className="rounded-[50px] sm:rounded-[20px]" alt="pic" />
+                <p className="sm:text-center ml-[100px] sm:leading-none text-white sm:text-[20px] mt-8 font-montserrat text-[25px] font-semibold">Zorro University</p>
               </div>
               <div className="max-w-[605px] ml-auto">
-                <Image src={"/assets/work02.png"} width={450} height={580} className="rounded-[50px] sm:rounded-[20px]" alt="pic" />
-                <p className="sm:text-center sm:leading-none sm:text-[20px] text-white mt-8 font-montserrat text-[25px] font-semibold ml-[137px]">Gemmie Skin Care</p>
+                <Image src={"/assets/work02.png"} width={350} height={0} className="rounded-[50px] sm:rounded-[20px]" alt="pic" />
+                <p className="sm:text-center sm:leading-none sm:text-[20px] text-white mt-8 font-montserrat text-[25px] font-semibold ml-[100px]">Gemmie Skin Care</p>
               </div>
               <div className="max-w-[605px]">
-                <Image src={"/assets/work03.png"} width={450} height={580} className="rounded-[50px] sm:rounded-[20px]" alt="pic" />
-                <p className="sm:text-center sm:text-[20px] text-white mt-8 font-montserrat text-[25px] font-semibold ml-[137px]">Doodle food</p>
+                <Image src={"/assets/work03.png"} width={350} height={0} className="rounded-[50px] sm:rounded-[20px]" alt="pic" />
+                <p className="sm:text-center sm:text-[20px] text-white mt-8 font-montserrat text-[25px] font-semibold ml-[100px]">Doodle food</p>
               </div>
               <div className="max-w-[605px] ml-auto">
-                <Image src={"/assets/work04.png"} width={450} height={580} className="rounded-[50px] sm:rounded-[20px]" alt="pic" />
-                <p className="sm:text-center sm:text-[20px] text-white mt-8 font-montserrat text-[25px] font-semibold ml-[137px]">Doodle food</p>
+                <Image src={"/assets/work04.png"} width={350} height={0} className="rounded-[50px] sm:rounded-[20px]" alt="pic" />
+                <p className="sm:text-center sm:text-[20px] text-white mt-8 font-montserrat text-[25px] font-semibold ml-[100px]">Doodle food</p>
               </div>
             </div>
             <div className='text-center'>
-              <button className="text-white mb-10 mt-6 text-[17px] bg-[#7B41F9] rounded-[70px] py-4 px-6 font-semibold">
+              <button className="text-white mb-10 mt-6 text-[17px] buttonv2 rounded-[70px] py-4 px-6 font-semibold">
                 Hire Us{" "}
                 <span>
                   <Image
@@ -74,9 +73,8 @@ const FeatureWork = ({ title }) => {
           </div>
         </div>
       </div>
-
     </>
   )
 }
 
-export default FeatureWork;
+export default FeatureWork
--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
 
     <>
-      <div className='sm:pb-[20px] sm:pt-[20px] sm:px-[20px] md:pb-[30px] md:pt-[30px] md:px-[30px] lg:pb-[40px] lg:pt-[50px] lg:px-[50px]  w-full bg-[#E3204A] #000213 relative z-[-2] text-white pb-[50px] pt-[70px] p-[100px] box-border' >
+      <div className='sm:pb-[20px] font-montserrat sm:pt-[20px] sm:px-[20px] md:pb-[30px] md:pt-[30px] md:px-[30px] lg:pb-[40px] lg:pt-[50px] lg:px-[50px]  w-full bg-[#7B41F9] #000213 relative z-[-2] text-white pb-[50px] pt-[70px] p-[100px] box-border' >
 
         <div className=''>
           <h1 className='sm:mb-[20px] sm:ml-0 sm:text-[30px] md:text-[40px] md:ml-0 lg:text-[40px] lg:ml-0 ml-[40px] mb-[40px] text-[#ffffff00] text-[80px] font-montserrat font-bold tracking-[1px]'
@@ -16,7 +16,7 @@ const Footer = () => {
               <h2 className='sm:leading-10 text-white font-thin text-[30px] leading-8 mb-[20px]'>let's build <br /> <span className='font-bold'>something awesome</span></h2>
               <p className='sm:mb-[10px] mb-[20px]'>Reach out to our team and we'll get back to you within a day.</p>
 
-              <div className='sm:gap-[15px] sm:flex-col md:flex-col lg:flex-col flex gap-[30px]'>
+              <div className='sm:gap-[15px] font-montserrat sm:flex-col md:flex-col lg:flex-col flex gap-[30px]'>
                 <div className='sm:mt-[5px] line h-[5px] w-[70px] bg-white mt-[10px]'></div>
                 <div className='flex flex-col'>
                   <p className='sm:mb-[5px] mb-[10px]'>Office M-13, The Curve Building, Sheikh Zayed Road, Dubai, UAE </p>
@@ -26,21 +26,21 @@ const Footer = () => {
 
                   <div className='flex items-center gap-[20px]'>
                     <Image src={"/assets/social/facebook.svg"} width={10} height={10} />
-                    <Image src={"/assets/social/instagram.svg"} width={10} height={10} />
-                    <Image src={"/assets/social/linkedin.svg"} width={10} height={10} />
-                    <Image src={"/assets/social/x-white.svg"} width={10} height={10} />
-                    <Image src={"/assets/social/youtube.svg"} width={10} height={10} />
+                    <Image src={"/assets/social/instagram.svg"} width={20} height={20} />
+                    <Image src={"/assets/social/linkedin.svg"} width={20} height={20} />
+                    <Image src={"/assets/social/x-white.svg"} width={20} height={20} />
+                    <Image src={"/assets/social/youtube.svg"} width={20} height={20} />
                   </div>
                 </div>
               </div>
 
             </div>
 
-            <div className='sm:flex-wrap sm:gap-[30px] right box-border flex gap-[50px]'>
-              <div>
+            <div className='sm:flex-wrap sm:gap-[80px] right box-border flex gap-[50px]'>
+              <div className='sm:text-center'>
                 <div className=' sm:mb-[20px] flex items-center gap-[5px] mb-[30px]'>
-                  <div className='h-[20px] w-[2px] bg-white'></div>
-                  <p>Overveiw</p>
+                  <div className='sm:hidden h-[20px] w-[2px] bg-white'></div>
+                  <p className='sm:flex sm:justify-center sm:m-auto'>Overveiw</p>
                 </div>
                 <p className='mb-[10px]'>About Us</p>
                 <p className='mb-[10px]'>Services</p>
@@ -51,10 +51,10 @@ const Footer = () => {
                 <p className='mb-[10px]'>Privacy Policy</p>
               </div>
 
-              <div>
+              <div className='sm:text-center'>
                 <div className='sm:mb-[20px] flex items-center gap-[5px] mb-[30px]'>
-                  <div className='h-[20px] w-[2px] bg-white'></div>
-                  <p>Web Solutions</p>
+                  <div className='sm:hidden h-[20px] w-[2px] bg-white'></div>
+                  <p className='sm:flex sm:justify-center sm:m-auto'>Web Solutions</p>
                 </div>
                 <p className='mb-[10px]'>About Us</p>
                 <p className='mb-[10px]'>Services</p>
@@ -65,10 +65,10 @@ const Footer = () => {
                 <p className='mb-[10px]'>Privacy Policy</p>
               </div>
 
-              <div>
+              <div className='sm:text-center'>
                 <div className='sm:mb-[20px] flex items-center gap-[5px] mb-[30px]'>
-                  <div className='h-[20px] w-[2px] bg-white'></div>
-                  <p>Digital Marketing Services</p>
+                  <div className='sm:hidden h-[20px] w-[2px] bg-white'></div>
+                  <p className='sm:flex sm:justify-center sm:m-auto'>Digital Marketing Services</p>
                 </div>
                 <p className='mb-[10px]'>About Us</p>
                 <p className='mb-[10px]'>Services</p>
