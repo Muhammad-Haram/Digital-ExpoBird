5cd96e04bd8a107d92f0849a5fe3466cf52383d3
Muhammad Haram <haramh643@gmail.com>
2024-04-05 06:21:58 +0100

    marquee updates

--- src/app/components/Service.jsx
+++ src/app/components/Service.jsx
@@ -5,15 +5,16 @@ import Link from 'next/link';
 const Service = () => {
   return (
     <Fragment>
+
       <div className="bg-[#ffffff] overflow-x-hidden font-montserrat">
-        <div className="pt-[4rem] md:pt-2 xl:px-36 px-10 relative z-10">
+        <div className="sm:px-5 sm:pt-[20px] pt-[4rem] md:pt-2 xl:px-36 px-10 relative z-10">
           <div className="py-[2rem]">
-            <h5 className="text-[40px] text-center font-bold font-montserrat text-[#252E48] md:flex md:justify-center">All your design tools in one place with <br /> one simple subscription</h5>
-            <p className='text-[20px] text-center text-[#545F7E]'>Your advanced marketing tool to save time, enhance your presence, and boost awareness.
+            <h5 className="sm:text-[25px] text-[40px] text-center font-bold font-montserrat text-[#252E48] md:flex md:justify-center">All your design tools <br /> in one place with one simple subscription</h5>
+            <p className='sm:text-[14px] sm:mt-[10px] text-[20px] text-center text-[#545F7E]'>Your advanced marketing tool to save time, enhance your presence, and boost awareness.
             </p>
           </div>
 
-          <div className=" grid grid-cols-12 sm:grid sm:grid-cols-1">
+          <div className="sm:justify-between grid grid-cols-12 sm:grid sm:grid-cols-1">
 
             <div className="col-span-6 left-container">
               <div className="md:pt-0 md:h-auto md:mb-[200px] lg:h-auto lg:mb-[200px]">
@@ -117,11 +118,17 @@ const Service = () => {
       </div>
 
 
+
+
+
+
+
       <Link href="/services/digital">
         <button className="buttonv2 mb-10 flex items-center m-auto justify-end px-6 py-4 rounded-[40px] text-[#fff] text-[17px] font-semibold md:flex md:items-left md:mx-auto">
           Get Started<span><Image src={"/assets/Send-arrow.svg"} className="inline" width={21} height={20} alt="pic" /></span>
         </button>
       </Link>
+
     </Fragment>
   )
 }
